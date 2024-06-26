a5b2658d3521e42ac1b2ee8150968cbb35ab3fcf
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-26 18:50:58 -0300

    comparando strings com o CompareTo

--- Program.cs
+++ Program.cs
@@ -17,10 +17,12 @@ namespace STRINGS
         var price = 10.2;
         //var texto = "o preço do produto é " + price;
        //var texto = string.Format("o preço do produto é {0} apenas na promoção", price);
-       var texto = $"O preço do produto \n é {price} apenas na promoção";
-        Console.WriteLine(texto);
+      //var texto = $"O preço do produto \n é {price} apenas na promoção";
+      //Console.WriteLine(texto);
 
-
+        var texto = "testando";
+        Console.WriteLine(texto.CompareTo("testando"));
+        
     }
   }  
 }
